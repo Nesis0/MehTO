e91248b18d89753af16d7a2bf96f522001ba2821
Itsiom <>
2024-10-13 16:20:01 +0200

    feat: implement basic navigation

--- app/src/main/java/com/isen/mehto/MainActivity.kt
+++ app/src/main/java/com/isen/mehto/MainActivity.kt
@@ -3,45 +3,15 @@ package com.isen.mehto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.isen.mehto.ui.theme.MehTOTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MehTOTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MehTOTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/isen/mehto/Navigation.kt
@@ -0,0 +1,107 @@
+package com.isen.mehto
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.isen.mehto.ui.models.HomeScreen
+import com.isen.mehto.ui.models.SettingsScreen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+data class DrawerMenu(val route: String, val screen: @Composable () -> Unit)
+
+val menuItems: Array<DrawerMenu> = arrayOf (
+    DrawerMenu("Home") { HomeScreen() },
+    DrawerMenu("Settings") { SettingsScreen() },
+)
+
+@Composable
+private fun DrawerContent(
+    menuItems: Array<DrawerMenu>,
+    onMenuClick: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        menuItems.forEach {
+            NavigationDrawerItem(
+                label = { Text(text = it.route) },
+                selected = false,
+                onClick = { onMenuClick(it.route) },
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomAppBar(drawerState: DrawerState) {
+    val coroutineScope = rememberCoroutineScope()
+
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { coroutineScope.launch { drawerState.open() } }) {
+                Icon(Icons.Filled.Menu, contentDescription = "")
+            }
+        },
+        title = { }
+    )
+}
+
+@Composable
+fun Navigation(
+    navController: NavHostController = rememberNavController(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+) {
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                DrawerContent(menuItems) { route ->
+                    coroutineScope.launch { drawerState.close() }
+                    navController.navigate(route)
+                }
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = { CustomAppBar( drawerState = drawerState ) }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(paddingValues),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                NavHost(navController = navController, startDestination = menuItems[0].route) {
+                    menuItems.forEach { item ->
+                        composable(item.route) { item.screen() }
+                    }
+                }
+            }
+        }
+    }
+}
