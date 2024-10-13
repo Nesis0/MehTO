4c3ed27c0a3a4fa50e79c3075f6c6a64a2d99fda
Nesis0 <alain.plaisance@outlook.fr>
2024-10-13 21:59:13 +0200

    feat: add possibility to search with a timerange

--- app/src/main/java/com/isen/mehto/api/WeatherService.kt
+++ app/src/main/java/com/isen/mehto/api/WeatherService.kt
@@ -13,4 +13,14 @@ class WeatherService {
             }
         }
     }
+    fun getWeather(timeRange: String, lat: String, lon: String) {
+        val httpRequests = HttpRequests()
+        httpRequests.get("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}") { response, error ->
+            if (error != null) {
+                println("Erreur lors de la requête GET : ${error.message}")
+            } else {
+                println("Réponse GET : $response")
+            }
+        }
+    }
 }
