c6d2c98dc06180cc4913253dbbd6a5cdb4802315
Nesis0 <alain.plaisance@outlook.fr>
2024-10-13 23:21:37 +0200

    style: remove useless parenthesis

--- app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
+++ app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
@@ -9,31 +9,28 @@ class WeatherService {
     fun getWeatherFromCity(city: String, country: String) {
         val (lat, lon) = getCoordinatesFromCity(city, country)
         httpRequests.get("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}") { response, error ->
-            if (error != null) {
+            if (error != null)
                 println("Erreur lors de la requête GET : ${error.message}")
-            } else {
+            else
                 println("Réponse GET : $response")
-            }
         }
     }
     fun getWeatherFromCity(timeRange: String, city: String, country: String) {
         val (lat, lon) = getCoordinatesFromCity(city, country)
         httpRequests.get("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}") { response, error ->
-            if (error != null) {
+            if (error != null)
                 println("Erreur lors de la requête GET : ${error.message}")
-            } else {
+            else
                 println("Réponse GET : $response")
-            }
         }
     }
 
     private fun getCoordinatesFromCity(city: String, country: String): Coordinates {
         httpRequests.get("http://api.openweathermap.org/geo/1.0/direct?q=$city,$country&limit=5&appid=$apiKey") { response, error ->
-            if (error != null) {
+            if (error != null)
                 println("Request error: ${error.message}")
-            } else {
+            else
                 println("GET response: $response")
-            }
         }
         return Coordinates("0","0")
     }
