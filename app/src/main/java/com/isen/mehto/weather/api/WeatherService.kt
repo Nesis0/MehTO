f42ac10dade94400d8a82fbfc2a0d694f78794dc
Nesis0 <alain.plaisance@outlook.fr>
2024-10-13 22:53:39 +0200

    feat: make httprequests an attribute

--- app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
+++ app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
@@ -2,9 +2,9 @@ package com.isen.mehto.weather.api
 
 class WeatherService {
     private val apiKey: String = "TOKEN"
+    private val httpRequests = HttpRequests()
 
     fun getWeather(lat: String, lon: String) {
-        val httpRequests = HttpRequests()
         httpRequests.get("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}") { response, error ->
             if (error != null) {
                 println("Erreur lors de la requête GET : ${error.message}")
@@ -14,7 +14,6 @@ class WeatherService {
         }
     }
     fun getWeather(timeRange: String, lat: String, lon: String) {
-        val httpRequests = HttpRequests()
         httpRequests.get("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}") { response, error ->
             if (error != null) {
                 println("Erreur lors de la requête GET : ${error.message}")
