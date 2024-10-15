e4e0ff3aa75dc93420b63d780744bb0e2e329f54
Nesis0 <alain.plaisance@outlook.fr>
2024-10-16 00:43:36 +0200

    feat: add city entity

---
+++ app/src/main/java/data/entity/City.kt
@@ -0,0 +1,20 @@
+package data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class City(
+    @PrimaryKey(autoGenerate = true)
+    val city_id: Int,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "latitude")
+    val latitude: Float,
+
+    @ColumnInfo(name = "longitude")
+    val longitude: Float
+)
