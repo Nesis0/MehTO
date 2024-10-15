d122a57800dbd8a2c5439941f4d8eb0e0e8f0648
Itsiom <>
2024-10-15 23:14:27 +0200

    feat: implement weather view for the coming week

--- app/src/main/java/com/isen/mehto/ui/viewmodels/WeatherViewModel.kt
+++ app/src/main/java/com/isen/mehto/ui/viewmodels/WeatherViewModel.kt
@@ -8,16 +8,24 @@ import com.isen.mehto.data.models.Weather
 import com.isen.mehto.data.repositories.WeatherRepository
 import com.isen.mehto.data.repositories.impl.WeatherRepositoryImpl
 import kotlinx.coroutines.launch
+import java.time.OffsetDateTime
 
 class WeatherViewModel() : ViewModel() {
     private val weatherRepository: WeatherRepository = WeatherRepositoryImpl();
 
-    private val _weather: MutableState<Weather?> = mutableStateOf(null)
-    val weather = _weather
+    private val _currentWeather: MutableState<Weather?> = mutableStateOf(null)
+    val currentWeather = _currentWeather
+    private val _weatherWeek: MutableState<Array<Weather?>> = mutableStateOf(arrayOfNulls(6))
+    val weatherWeek = _weatherWeek
 
     init {
         viewModelScope.launch {
-            _weather.value = weatherRepository.getWeather()
+            _currentWeather.value = weatherRepository.getWeather()
+
+            for (i in _weatherWeek.value.indices) {
+                val date = OffsetDateTime.now().plusDays(i.toLong())
+                _weatherWeek.value[i] = weatherRepository.getWeather(date)
+            }
         }
     }
 }
--- app/src/main/java/com/isen/mehto/ui/views/WeatherView.kt
+++ app/src/main/java/com/isen/mehto/ui/views/WeatherView.kt
@@ -3,27 +3,48 @@ package com.isen.mehto.ui.views
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.isen.mehto.R
+import com.isen.mehto.ui.theme.DashedDivider
 import com.isen.mehto.ui.viewmodels.WeatherViewModel
 
 @Composable
 fun WeatherScreen() {
     val viewModel: WeatherViewModel = viewModel()
 
-    if (viewModel.weather.value == null) {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CurrentWeather(viewModel)
+        Spacer(modifier = Modifier.height(30.dp))
+        WeatherWeek(viewModel)
+    }
+}
+
+@Composable
+private fun CurrentWeather(viewModel: WeatherViewModel) {
+    if (viewModel.currentWeather.value == null) {
         Text(text = "Unable to show weather")
     }
 
-    viewModel.weather.value?.let {
+    viewModel.currentWeather.value?.let {
         Column(
-            Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -33,6 +54,46 @@ fun WeatherScreen() {
             )
             Text(fontSize = 32.sp, text = "${it.temperature.inCelsius} °C")
             Text(fontSize = 24.sp, text = it.city)
+        }
+    }
+}
+
+@Composable
+private fun WeatherWeek(viewModel: WeatherViewModel) {
+    Column {
+        if (viewModel.weatherWeek.value[0] != null) DashedDivider()
+
+        for (weather in viewModel.weatherWeek.value) {
+            weather?.let {
+                Row(
+                    Modifier
+                        .height(50.dp)
+                        .fillMaxWidth(0.9f)
+                        .align(Alignment.CenterHorizontally),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Image(
+                            modifier = Modifier.fillMaxHeight(0.8f),
+                            painter = painterResource(id = it.weatherType.image),
+                            contentDescription = "Weather Icon",
+                        )
+                        Spacer(modifier = Modifier.width(20.dp))
+                        Text(fontSize = 26.sp, text = "${it.temperature.inCelsius} °C")
+                    }
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(fontSize = 26.sp, text = "${it.rainRisk}")
+                        Image(
+                            modifier = Modifier.fillMaxHeight(0.8f),
+                            painter = painterResource(id = R.drawable.ic_rainrisk),
+                            contentDescription = "Rain Risk Icon",
+                        )
+                    }
+                }
+                DashedDivider()
+            }
         }
     }
 }
