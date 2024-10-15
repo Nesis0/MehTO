08e6f6aeb4b2c1e56cde8e77347d3dbe6eb95020
Nesis0 <alain.plaisance@outlook.fr>
2024-10-16 00:44:27 +0200

    feat: add city repository

---
+++ app/src/main/java/data/CityRepository.kt
@@ -0,0 +1,18 @@
+package data
+
+import data.entity.City
+
+interface CityRepository {
+    suspend fun getAllCities(): List<City>
+
+    suspend fun insertCity(city: City)
+
+    suspend fun getCityInfo(cityName: String): City?
+
+    suspend fun updateCity(city: City)
+
+    suspend fun deleteCity(city: City)
+
+    suspend fun clearCityList()
+
+}
