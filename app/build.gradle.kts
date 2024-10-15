f1a85777efb53a8faf2dc1a0563efe3a6042a71e
Nesis0 <alain.plaisance@outlook.fr>
2024-10-15 21:43:19 +0200

    build: add retrofit and gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
 }
 
 dependencies {
-    implementation(libs.okhttp)
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.converter.gson)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
