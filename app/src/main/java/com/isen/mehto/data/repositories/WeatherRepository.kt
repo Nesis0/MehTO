e80c2e0620766975eb24d3941e5b2363f80f6c50
Itsiom <>
2024-10-15 22:08:28 +0200

    feat: implement getWeather with no date input

--- app/src/main/java/com/isen/mehto/data/repositories/WeatherRepository.kt
+++ app/src/main/java/com/isen/mehto/data/repositories/WeatherRepository.kt
@@ -1,9 +1,9 @@
 package com.isen.mehto.data.repositories
 
 import com.isen.mehto.data.models.Weather
-import java.time.LocalDateTime
+import java.time.OffsetDateTime
 
 interface WeatherRepository {
     suspend fun getWeather(): Weather
-    suspend fun getWeather(time: LocalDateTime): Weather
+    suspend fun getWeather(time: OffsetDateTime): Weather
 }
--- app/src/main/java/com/isen/mehto/data/repositories/impl/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/isen/mehto/data/repositories/impl/WeatherRepositoryImpl.kt
@@ -1,15 +1,18 @@
 package com.isen.mehto.data.repositories.impl
 
+import android.health.connect.datatypes.units.Percentage
+import android.health.connect.datatypes.units.Temperature
 import com.isen.mehto.data.models.Weather
+import com.isen.mehto.data.models.WeatherType
 import com.isen.mehto.data.repositories.WeatherRepository
-import java.time.LocalDateTime
+import java.time.OffsetDateTime
 
 class WeatherRepositoryImpl() : WeatherRepository {
     override suspend fun getWeather(): Weather {
-        TODO("Not yet implemented")
+        return getWeather(OffsetDateTime.now())
     }
 
-    override suspend fun getWeather(time: LocalDateTime): Weather {
+    override suspend fun getWeather(time: OffsetDateTime): Weather {
         TODO("Not yet implemented")
     }
 }
