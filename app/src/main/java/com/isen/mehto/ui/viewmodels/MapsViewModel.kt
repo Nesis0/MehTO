24eb2be48af4d834dd52ad06d21246be5218d2c5
Itsiom <>
2024-10-16 21:10:25 +0200

    feat: add drop down for map selection

--- app/src/main/java/com/isen/mehto/ui/viewmodels/MapsViewModel.kt
+++ app/src/main/java/com/isen/mehto/ui/viewmodels/MapsViewModel.kt
@@ -1,10 +1,16 @@
 package com.isen.mehto.ui.viewmodels
 
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class MapsViewModel() : ViewModel() {
+    var items = listOf("Precipitations", "Temperatures", "Winds", "Pressures", "Air Quality")
+    val expanded = mutableStateOf(false)
+    val selectedIndex = mutableIntStateOf(0)
+
     init {
         viewModelScope.launch {
 
