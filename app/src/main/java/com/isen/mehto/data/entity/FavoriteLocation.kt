cdadb9a0c025af954f681b87cc2c3d4beedbffb5
Nesis0 <alain.plaisance@outlook.fr>
2024-10-17 19:42:38 +0200

    feat: add preference index col in location table

--- app/src/main/java/com/isen/mehto/data/entity/FavoriteLocation.kt
+++ app/src/main/java/com/isen/mehto/data/entity/FavoriteLocation.kt
@@ -9,6 +9,9 @@ data class FavoriteLocation(
     @PrimaryKey(autoGenerate = true)
     val location_id: Int,
 
+    @ColumnInfo(name = "preference_index")
+    val preference_index: Int,
+
     @ColumnInfo(name = "display_name")
     val display_name: String,
 
