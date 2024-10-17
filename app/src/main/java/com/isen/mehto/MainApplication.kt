b9560d7a3c10e3c2f0f234fad5012d5a873bb9fa
Nesis0 <alain.plaisance@outlook.fr>
2024-10-17 22:52:28 +0200

    feat: instanciate apiService with koin

--- app/src/main/java/com/isen/mehto/MainApplication.kt
+++ app/src/main/java/com/isen/mehto/MainApplication.kt
@@ -7,6 +7,7 @@ import com.isen.mehto.data.OfflineFavoriteLocationRepository
 import com.isen.mehto.data.db.LocalDatabase
 import com.isen.mehto.data.repositories.WeatherRepository
 import com.isen.mehto.data.repositories.impl.WeatherRepositoryImpl
+import com.isen.mehto.weather.api.WeatherCall
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
@@ -39,4 +40,6 @@ val databaseModule = module {
     single { get<LocalDatabase>().configDAO() }
 }
 
-val apiService = module { }
+val apiService = module {
+    single { WeatherCall.service }
+}
