c14713a2338e5a2af9dfe1d375d53941c3298ea1
Nesis0 <alain.plaisance@outlook.fr>
2024-10-17 23:44:57 +0200

    fix: typo in api url

--- app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
+++ app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
@@ -10,7 +10,7 @@ data class CityInfo(val weather: String, val temp: String)
 const val API_KEY: String = "63b335fbcbab900e14a56296d96716b4"
 
 interface WeatherService {
-    @GET("data/2.5/weather?lat{latitude}&lon={longitude}&appid=$API_KEY")
+    @GET("data/2.5/weather?lat={latitude}&lon={longitude}&appid=$API_KEY")
     suspend fun getTodayWeather(@Path("latitude") latitude: Float, @Path("longitude") longitude: Float): Weather
 
     @GET("geo/1.0/direct?q={city},{country}&limit=5&appid=$API_KEY")
