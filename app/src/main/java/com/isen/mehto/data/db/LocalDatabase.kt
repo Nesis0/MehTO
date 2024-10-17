f4f7da495cd1845a288a2800fb2e7833bb2c3f2c
Nesis0 <alain.plaisance@outlook.fr>
2024-10-17 22:05:25 +0200

    feat: add callback to prepopulate database

--- app/src/main/java/com/isen/mehto/data/db/LocalDatabase.kt
+++ app/src/main/java/com/isen/mehto/data/db/LocalDatabase.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.isen.mehto.data.entity.FavoriteLocation
 import com.isen.mehto.data.entity.FavoriteLocationDAO
 import com.isen.mehto.data.entity.Config
 import com.isen.mehto.data.entity.ConfigDAO
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(version = 1, entities = [Config::class, FavoriteLocation::class], exportSchema = false)
 abstract class LocalDatabase : RoomDatabase() {
@@ -22,9 +26,24 @@ abstract class LocalDatabase : RoomDatabase() {
           fun getDatabase(context: Context): LocalDatabase {
                return Instance ?: synchronized(this) {
                     Room.databaseBuilder(context, LocalDatabase::class.java,"LocalDatabase")
+                         .addCallback(object : RoomDatabase.Callback() {
+                              override fun onCreate(db: SupportSQLiteDatabase) {
+                                   super.onCreate(db)
+                                   populateDatabase(getDatabase(context).configDAO())
+                              }
+                         })
                          .build()
                          .also { Instance = it}
                }
           }
+
+          fun populateDatabase(configDAO: ConfigDAO) {
+               CoroutineScope(Dispatchers.IO).launch {
+                    configDAO.insert(Config("unit", "Celsius"))
+                    configDAO.insert(Config("Language", "FR"))
+                    configDAO.insert(Config("Location", "true"))
+               }
+          }
      }
+
 }
