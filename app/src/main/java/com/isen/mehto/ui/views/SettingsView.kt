d4a737fd3315a54735afaa72e9ae7d9c267ac384
Nesis0 <alain.plaisance@outlook.fr>
2024-10-17 15:54:37 +0200

    feat: add settings viewmodel module

--- app/src/main/java/com/isen/mehto/ui/views/SettingsView.kt
+++ app/src/main/java/com/isen/mehto/ui/views/SettingsView.kt
@@ -2,8 +2,14 @@ package com.isen.mehto.ui.views
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.isen.mehto.ui.viewmodels.SettingsViewModel
+import org.koin.androidx.compose.get
 
 @Composable
 fun SettingsScreen() {
+    val viewModel = viewModel<SettingsViewModel>(
+        factory = SettingsViewModel.ViewModelFactory(get())
+    )
     Text(text = "Settings Screen")
 }
