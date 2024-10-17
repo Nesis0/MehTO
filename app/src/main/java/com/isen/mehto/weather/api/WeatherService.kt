4ecf88ea5d3540dbb17189870f83cf0c6f763dbe
Nesis0 <alain.plaisance@outlook.fr>
2024-10-17 22:46:14 +0200

    style: change api key variable name to match conventional constant naming

--- app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
+++ app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
@@ -5,12 +5,12 @@ import retrofit2.http.Path
 
 data class CityInfo(val weather: String, val temp: String)
 data class Coordinates(val latitude: String, val longitude: String)
-const val apiKey: String = "63b335fbcbab900e14a56296d96716b4"
+const val API_KEY: String = "63b335fbcbab900e14a56296d96716b4"
 
 interface WeatherService {
-    @GET("data/2.5/weather?lat{latitude}&lon={longitude}&appid=$apiKey")
+    @GET("data/2.5/weather?lat{latitude}&lon={longitude}&appid=$API_KEY")
     suspend fun getTodayWeather(@Path("latitude") latitude: String, @Path("longitude") longitude: String): CityInfo
 
-    @GET("geo/1.0/direct?q={city},{country}&limit=5&appid=$apiKey")
+    @GET("geo/1.0/direct?q={city},{country}&limit=5&appid=$API_KEY")
     suspend fun getCoordinatesFromCity(@Path("city") city: String, @Path("country") country: String): Coordinates
 }
