2561a8722387e97883ff03b7c8091014ef4cd40d
Nesis0 <alain.plaisance@outlook.fr>
2024-10-17 22:47:47 +0200

    refactor: return position object in getcoordinates api

--- app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
+++ app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
@@ -1,10 +1,11 @@
 package com.isen.mehto.weather.api
 
+import com.isen.mehto.data.models.Position
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 data class CityInfo(val weather: String, val temp: String)
-data class Coordinates(val latitude: String, val longitude: String)
+
 const val API_KEY: String = "63b335fbcbab900e14a56296d96716b4"
 
 interface WeatherService {
@@ -12,5 +13,5 @@ interface WeatherService {
     suspend fun getTodayWeather(@Path("latitude") latitude: String, @Path("longitude") longitude: String): CityInfo
 
     @GET("geo/1.0/direct?q={city},{country}&limit=5&appid=$API_KEY")
-    suspend fun getCoordinatesFromCity(@Path("city") city: String, @Path("country") country: String): Coordinates
+    suspend fun getCoordinatesFromCity(@Path("city") city: String, @Path("country") country: String): Position
 }
--- app/src/main/java/com/isen/mehto/weather/api/WeatherServiceImpl.kt
+++ app/src/main/java/com/isen/mehto/weather/api/WeatherServiceImpl.kt
@@ -1,11 +1,13 @@
 package com.isen.mehto.weather.api
 
+import com.isen.mehto.data.models.Position
+
 class WeatherServiceImpl: WeatherService {
     override suspend fun getTodayWeather(latitude: String, longitude: String): CityInfo {
         return WeatherCall.service.getTodayWeather(latitude, longitude)
     }
 
-    override suspend fun getCoordinatesFromCity(city: String, country: String): Coordinates {
+    override suspend fun getCoordinatesFromCity(city: String, country: String): Position {
         return WeatherCall.service.getCoordinatesFromCity(city, country)
     }
 }
