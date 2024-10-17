3ab3b26eea64adab3b722e97ef73b7365b3076ac
Nesis0 <alain.plaisance@outlook.fr>
2024-10-17 23:50:54 +0200

    refactor: clear useless imports

--- app/src/main/java/com/isen/mehto/data/repositories/WeatherRepository.kt
+++ app/src/main/java/com/isen/mehto/data/repositories/WeatherRepository.kt
@@ -2,7 +2,6 @@ package com.isen.mehto.data.repositories
 
 import com.isen.mehto.data.models.Position
 import com.isen.mehto.data.models.Weather
-import java.time.OffsetDateTime
 
 interface WeatherRepository {
     suspend fun getTodayWeather(position: Position): Weather
--- app/src/main/java/com/isen/mehto/data/repositories/impl/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/isen/mehto/data/repositories/impl/WeatherRepositoryImpl.kt
@@ -1,13 +1,9 @@
 package com.isen.mehto.data.repositories.impl
 
-import android.health.connect.datatypes.units.Percentage
-import android.health.connect.datatypes.units.Temperature
 import com.isen.mehto.data.models.Position
 import com.isen.mehto.data.models.Weather
-import com.isen.mehto.data.models.WeatherType
 import com.isen.mehto.data.repositories.WeatherRepository
 import com.isen.mehto.weather.api.WeatherServiceImpl
-import java.time.OffsetDateTime
 
 class WeatherRepositoryImpl(private val weatherService: WeatherServiceImpl) : WeatherRepository {
     override suspend fun getTodayWeather(position: Position): Weather {
