bc18c84d3ebdd97cbb94347dcc9d84f1533519fb
Nesis0 <alain.plaisance@outlook.fr>
2024-10-18 20:43:14 +0200

    refactor: use query annotation in request

--- app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
+++ app/src/main/java/com/isen/mehto/weather/api/WeatherService.kt
@@ -3,19 +3,19 @@ package com.isen.mehto.weather.api
 import com.isen.mehto.data.models.Position
 import com.isen.mehto.data.models.Weather
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 data class CityInfo(val weather: String, val temp: String)
 
 const val API_KEY: String = "63b335fbcbab900e14a56296d96716b4"
 
 interface WeatherService {
-    @GET("data/2.5/weather?lat={latitude}&lon={longitude}&appid=$API_KEY")
-    suspend fun getTodayWeather(@Path("latitude") latitude: Float, @Path("longitude") longitude: Float): Weather
+    @GET("data/2.5/weather")
+    suspend fun getTodayWeather(@Query("lat") latitude: Float, @Query("lon") longitude: Float, @Query("appid") apiKey: String? = API_KEY): Weather
 
-    @GET("/data/2.5/forecast?lat={latitude}&lon={longitude}&appid=$API_KEY")
-    suspend fun getForecast(@Path("latitude") latitude: Float, @Path("longitude") longitude: Float): Weather
+    @GET("/data/2.5/forecast")
+    suspend fun getForecast(@Query("lat") latitude: Float, @Query("lon") longitude: Float, @Query("appid") apiKey: String? = API_KEY): Weather
 
-    @GET("geo/1.0/direct?q={city}&limit=5&appid=$API_KEY")
-    suspend fun getCoordinatesFromCity(@Path("city") city: String): Position
+    @GET("geo/1.0/direct")
+    suspend fun getCoordinatesFromCity(@Query("q") city: String, @Query("appid") apiKey: String? = API_KEY): Position
 }
--- app/src/main/java/com/isen/mehto/weather/api/WeatherServiceImpl.kt
+++ app/src/main/java/com/isen/mehto/weather/api/WeatherServiceImpl.kt
@@ -6,13 +6,14 @@ import com.isen.mehto.data.models.Weather
 class WeatherServiceImpl: WeatherService {
     override suspend fun getTodayWeather(latitude: Float, longitude: Float): Weather {
         return WeatherCall.service.getTodayWeather(latitude, longitude)
+    override suspend fun getTodayWeather(latitude: Float, longitude: Float, apiKey: String?): Weather {
     }
 
-    override suspend fun getForecast(latitude: Float, longitude: Float): Weather {
+    override suspend fun getForecast(latitude: Float, longitude: Float, apiKey: String?): Weather {
         return WeatherCall.service.getForecast(latitude, longitude)
     }
 
-    override suspend fun getCoordinatesFromCity(city: String): Position {
+    override suspend fun getCoordinatesFromCity(city: String, apiKey: String?): Position {
         return WeatherCall.service.getCoordinatesFromCity(city)
     }
 }
