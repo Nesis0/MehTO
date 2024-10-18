de3b253372148a601be265a04bba1b0ca7796897
Nesis0 <alain.plaisance@outlook.fr>
2024-10-18 23:41:23 +0200

    feat: make getTodayWeather in repository return Weather object

--- app/src/main/java/com/isen/mehto/data/repositories/impl/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/isen/mehto/data/repositories/impl/WeatherRepositoryImpl.kt
@@ -1,13 +1,19 @@
 package com.isen.mehto.data.repositories.impl
 
+import android.health.connect.datatypes.units.Temperature
 import com.isen.mehto.data.models.Position
 import com.isen.mehto.data.models.Weather
+import com.isen.mehto.data.models.WeatherResponse
+import com.isen.mehto.data.models.WeatherType
 import com.isen.mehto.data.repositories.WeatherRepository
 import com.isen.mehto.weather.api.WeatherServiceImpl
 
 class WeatherRepositoryImpl(private val weatherService: WeatherServiceImpl) : WeatherRepository {
     override suspend fun getTodayWeather(position: Position): Weather {
-        return weatherService.getTodayWeather(position.lat, position.lon)
+        val weatherResponse: WeatherResponse = weatherService.getTodayWeather(position.lat, position.lon)
+        val weather: Weather = mapWeatherFromWeatherResponse(weatherResponse)
+
+        return weather
     }
 
     override suspend fun getForecast(position: Position): Weather {
