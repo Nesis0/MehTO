30747d8a5d5209693d7797324526abc882a2a5c5
Nesis0 <alain.plaisance@outlook.fr>
2024-10-19 00:09:51 +0200

    feat: add temperature class

---
+++ app/src/main/java/com/isen/mehto/data/models/Temperature.kt
@@ -0,0 +1,36 @@
+package com.isen.mehto.data.models
+
+enum class TemperatureUnit {
+    KELVIN,
+    CELSIUS,
+    FAHRENHEIT,
+
+}
+
+class Temperature(private val value: Double, private val unit: TemperatureUnit){
+    fun toCelsius(): Double {
+        return when(this.unit){
+            TemperatureUnit.KELVIN -> value - 273.15
+            TemperatureUnit.FAHRENHEIT -> (this.value - 32) * 5 / 9
+            else -> this.value
+        }
+    }
+
+    fun toFahrenheit(): Double {
+        return when (this.unit) {
+            TemperatureUnit.CELSIUS -> (this.value * 9 / 5) + 32
+            TemperatureUnit.KELVIN -> (this.value - 273.15) * 9 / 5 + 32
+            else -> this.value
+        }
+    }
+
+    fun toKelvin(): Double {
+        return when (this.unit) {
+            TemperatureUnit.CELSIUS -> this.value + 273.15
+            TemperatureUnit.FAHRENHEIT -> (this.value - 32) * 5 / 9 + 273.15
+            else -> this.value
+        }
+    }
+
+
+}
