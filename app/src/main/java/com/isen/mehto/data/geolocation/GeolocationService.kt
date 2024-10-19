7ab37c2687bb7a4844140f09076394282d8450f4
Nesis0 <alain.plaisance@outlook.fr>
2024-10-19 23:41:57 +0200

    feat: check for geolocation permission

--- app/src/main/java/com/isen/mehto/data/geolocation/GeolocationService.kt
+++ app/src/main/java/com/isen/mehto/data/geolocation/GeolocationService.kt
@@ -1,15 +1,31 @@
 package com.isen.mehto.data.geolocation
 
+import android.Manifest
 import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class GeolocationService(private val context: Context, private val activity: Activity) {
     private val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
 
     fun getLastLocation(onSuccess: (Location?) -> Unit) {
-        fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+        if (!checkForPermission())
+            askForPermission()
+
+        fusedLocationClient.lastLocation.addOnSuccessListener {
             location: Location? -> onSuccess(location)
         }
+    }
+
+    private fun checkForPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun askForPermission(){
+        ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
     }
 }
