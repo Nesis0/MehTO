20adecfe9f67c9a39c4517855cafbbc2cbc5c667
Itsiom <>
2024-10-19 22:04:33 +0200

    feat: add position picker from text input

--- app/src/main/java/com/isen/mehto/ui/theme/Theme.kt
+++ app/src/main/java/com/isen/mehto/ui/theme/Theme.kt
@@ -1,47 +1,27 @@
 package com.isen.mehto.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+private val ColorScheme = lightColorScheme(
+    primary = Color.Black,
+    secondary = Color.Black,
+    tertiary = Color.Black,
     background = Blue60,
     surface = Color.Gray.copy(alpha = 0.9f),
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    background = Blue60,
-    surface = Color.Gray.copy(alpha = 0.9f),
-    /* Other default colors to override
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
-fun MehTOTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
-) {
-    val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
+fun MehTOTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = ColorScheme,
         typography = Typography,
         content = content
     )
--- app/src/main/java/com/isen/mehto/ui/views/FavoriteLocationView.kt
+++ app/src/main/java/com/isen/mehto/ui/views/FavoriteLocationView.kt
@@ -1,8 +1,21 @@
 package com.isen.mehto.ui.views
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
@@ -12,8 +25,14 @@ import com.isen.mehto.data.models.Position
 import com.isen.mehto.viewmodels.FavoriteLocationViewModel
 import org.koin.androidx.compose.get
 
+
 @Composable
 fun FavoriteLocationScreen() {
+    AddFavoriteLocationScreen()
+}
+
+@Composable
+fun AddFavoriteLocationScreen() {
     val viewModel = viewModel<FavoriteLocationViewModel>(
         factory = FavoriteLocationViewModel.ViewModelFactory(get())
     )
@@ -26,7 +45,30 @@ fun FavoriteLocationScreen() {
         )
     }
 
-    Column {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = viewModel.userInput.value,
+            onValueChange = { viewModel.userInput.value = it },
+            label = { Text("Search location") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(0.9f),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = Color.Gray.copy(alpha = 0.7f),
+                focusedContainerColor = Color.Gray.copy(alpha = 0.7f),
+                unfocusedBorderColor = Color.Black,
+            ),
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon",
+                )
+            },
+        )
+
+        Spacer(Modifier.height(20.dp))
+
         GoogleMap(cameraPositionState = cameraPositionState) {
             Marker(state = MarkerState(position = cameraPositionState.position.target))
         }
