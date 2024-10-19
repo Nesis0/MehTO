b2928c386a4a494a440eef0c74687f576522218f
Itsiom <>
2024-10-19 16:48:21 +0200

    feat: show week forecast in double bordered box

--- app/src/main/java/com/isen/mehto/ui/views/ForecastView.kt
+++ app/src/main/java/com/isen/mehto/ui/views/ForecastView.kt
@@ -2,7 +2,9 @@ package com.isen.mehto.ui.views
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,19 +12,24 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.isen.mehto.R
-import com.isen.mehto.ui.theme.DashedDivider
+import com.isen.mehto.data.models.Forecast
 import com.isen.mehto.viewmodels.ForecastViewModel
 import org.koin.androidx.compose.get
+import kotlin.math.roundToInt
 
 @Composable
 fun ForecastScreen() {
@@ -63,43 +70,54 @@ private fun CurrentForecast(viewModel: ForecastViewModel) {
     }
 }
 
-@SuppressLint("DefaultLocale")
 @Composable
 private fun ForecastWeek(viewModel: ForecastViewModel) {
-    Column {
-        if (viewModel.weatherWeek.value.isNotEmpty()) DashedDivider()
+    val blackBorder = Modifier
+        .border(width = 2.dp, color = Color.Black, shape = RectangleShape)
+        .padding(5.dp)
 
-        for (weather in viewModel.weatherWeek.value) {
-            weather.let {
-                Row(
-                    Modifier
-                        .height(50.dp)
-                        .fillMaxWidth(0.9f)
-                        .align(Alignment.CenterHorizontally),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                ) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Image(
-                            modifier = Modifier.fillMaxHeight(0.8f),
-                            painter = painterResource(id = it.weatherConditions.image),
-                            contentDescription = "Weather Icon",
-                        )
-                        Spacer(modifier = Modifier.width(20.dp))
-                        Text(fontSize = 26.sp, text = "${String.format("%.2f", it.temperature.toCelsius())} °C")
-                    }
-
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(fontSize = 26.sp, text = "${it.humidity}")
-                        Image(
-                            modifier = Modifier.fillMaxHeight(0.8f),
-                            painter = painterResource(id = R.drawable.ic_humidity),
-                            contentDescription = "Rain Risk Icon",
-                        )
-                    }
+    Box(modifier = blackBorder.fillMaxWidth(0.95f)) {
+        Column(modifier = blackBorder, horizontalAlignment = Alignment.CenterHorizontally) {
+            viewModel.weatherWeek.value.forEachIndexed { index, weather ->
+                weather.let {
+                    ForecastItem(it)
+                    if (viewModel.weatherWeek.value.size != index + 1) HorizontalDivider(color = Color.Black)
                 }
-                DashedDivider()
             }
+        }
+    }
+}
+
+@SuppressLint("DefaultLocale")
+@Composable
+private fun ForecastItem(it: Forecast) {
+    Row(
+        Modifier
+            .height(50.dp)
+            .fillMaxWidth(0.9f),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                modifier = Modifier.fillMaxHeight(0.8f),
+                painter = painterResource(id = it.weatherConditions.image),
+                contentDescription = "Weather Icon",
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                fontSize = 26.sp,
+                text = "${String.format("%.2f", it.temperature.toCelsius())} °C"
+            )
+        }
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(fontSize = 26.sp, text = "${it.humidity.value.roundToInt()}%")
+            Image(
+                modifier = Modifier.fillMaxHeight(0.8f),
+                painter = painterResource(id = R.drawable.ic_humidity),
+                contentDescription = "Rain Risk Icon",
+            )
         }
     }
 }
