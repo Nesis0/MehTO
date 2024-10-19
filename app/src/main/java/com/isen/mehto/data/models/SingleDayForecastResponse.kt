7cda58fd202e45e3f00406bb57278db2c93e78ac
Nesis0 <alain.plaisance@outlook.fr>
2024-10-19 02:19:32 +0200

    refactor: change WeatherCondition to ForecastInfos

--- app/src/main/java/com/isen/mehto/data/models/SingleDayForecastResponse.kt
+++ app/src/main/java/com/isen/mehto/data/models/SingleDayForecastResponse.kt
@@ -1,13 +1,13 @@
 package com.isen.mehto.data.models
 
 data class SingleDayForecastResponse(
-    val weather: List<WeatherCondition>,
+    val weather: List<ForecastInfos>,
     val main: Infos,
     var name: String,
     val dt_txt: String
 )
 
-data class WeatherCondition(
+data class ForecastInfos(
     val main: String
 )
 
