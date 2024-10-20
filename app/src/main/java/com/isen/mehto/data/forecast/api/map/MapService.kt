b21a3086216f02d5af2fd659d79ec5788ff373f7
Nesis0 <alain.plaisance@outlook.fr>
2024-10-20 18:23:49 +0200

    feat: add requests in map service

--- app/src/main/java/com/isen/mehto/data/forecast/api/map/MapService.kt
+++ app/src/main/java/com/isen/mehto/data/forecast/api/map/MapService.kt
@@ -1,19 +1,28 @@
 package com.isen.mehto.data.forecast.api.map
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
 const val API_KEY: String = "63b335fbcbab900e14a56296d96716b4"
+const val ZOOM_LEVEL: String = "3"
+const val X_TILES: String = "3"
+const val Y_TILES: String = "3"
 
 interface MapService {
-    suspend fun getPrecipitationMap()
+    @GET("map/precipitation_new/{z}/{x}/{y}.png")
+    suspend fun getPrecipitationMap(@Path("z") zoomLevel: String? = ZOOM_LEVEL, @Path("x") xTiles: String? = X_TILES, @Path("y") yTiles: String? = Y_TILES, @Query("appid") apiKey: String? = API_KEY)
 
-    suspend fun getTemperaturesMap()
+    @GET("map/temp_new/{z}/{x}/{y}.png")
+    suspend fun getTemperaturesMap(@Path("z") zoomLevel: String? = ZOOM_LEVEL, @Path("x") xTiles: String? = X_TILES, @Path("y") yTiles: String? = Y_TILES, @Query("appid") apiKey: String? = API_KEY)
 
-    suspend fun getPressureMap()
+    @GET("map/pressure_new/{z}/{x}/{y}.png")
+    suspend fun getPressureMap(@Path("z") zoomLevel: String? = ZOOM_LEVEL, @Path("x") xTiles: String? = X_TILES, @Path("y") yTiles: String? = Y_TILES, @Query("appid") apiKey: String? = API_KEY)
 
-    suspend fun getWindsMap()
+    @GET("map/wind_new/{z}/{x}/{y}.png")
+    suspend fun getWindsMap(@Path("z") zoomLevel: String? = ZOOM_LEVEL, @Path("x") xTiles: String? = X_TILES, @Path("y") yTiles: String? = Y_TILES, @Query("appid") apiKey: String? = API_KEY)
 
-    suspend fun getCloudMap()
+    @GET("map/clouds_new/{z}/{x}/{y}.png")
+    suspend fun getCloudMap(@Path("z") zoomLevel: String? = ZOOM_LEVEL, @Path("x") xTiles: String? = X_TILES, @Path("y") yTiles: String? = Y_TILES, @Query("appid") apiKey: String? = API_KEY)
 }
