61f2fc02c67b1b9264f7d988532f54007cf5e84c
Nesis0 <alain.plaisance@outlook.fr>
2024-10-20 22:25:17 +0200

    feat: make repository return bitmap

--- app/src/main/java/com/isen/mehto/data/repositories/api/map/MapRepository.kt
+++ app/src/main/java/com/isen/mehto/data/repositories/api/map/MapRepository.kt
@@ -1,9 +1,11 @@
 package com.isen.mehto.data.repositories.api.map
 
+import android.graphics.Bitmap
+
 interface MapRepository {
-    suspend fun getPrecipitationMap()
-    suspend fun getTemperaturesMap()
-    suspend fun getPressureMap()
-    suspend fun getWindsMap()
-    suspend fun getCloudMap()
+    suspend fun getPrecipitationMap(): Bitmap
+    suspend fun getTemperaturesMap(): Bitmap
+    suspend fun getPressureMap(): Bitmap
+    suspend fun getWindsMap(): Bitmap
+    suspend fun getCloudMap(): Bitmap
 }
